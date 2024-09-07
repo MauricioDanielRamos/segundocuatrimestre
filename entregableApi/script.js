8c848ff13d0273670af11a420820f8c0f1365388
MauricioDanielRamos <168040388+MauricioDanielRamos@users.noreply.github.com>
2024-09-07 14:39:30 -0300

    primer entregable segundo cuatrimestre API

---
+++ entregableApi/script.js
@@ -0,0 +1,15 @@
+let contenedor = document.getElementById("contenedor");
+
+fetch("https://fakestoreapi.com/products")
+.then(resp => resp.json())
+.then(data => {
+    data.forEach(products => {
+        contenedor.innerHTML += `<div class="card">
+                                    <h2>Nombre ${products.title}</h2>
+                                    <p>${products.category}</p>
+                                    <img src="${products.image}" alt="product image">
+                                    <h4>Precio: $ ${products.price.toFixed(2)}</h4> 
+                                </div>`        // Agregue toFixed(2) para que los precios queden con decimales
+    })
+})
+.catch(err => console.log(err));
