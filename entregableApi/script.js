53b8d3eb23c3523e0bf65afa75f47a0c44e7bf78
MauricioDanielRamos <168040388+MauricioDanielRamos@users.noreply.github.com>
2024-09-11 22:18:19 -0300

    cambios: outline en hover y alt en img

--- entregableApi/script.js
+++ entregableApi/script.js
@@ -7,7 +7,7 @@ fetch("https://fakestoreapi.com/products")
         contenedor.innerHTML += `<div class="card">
                                     <h2>Nombre ${products.title}</h2>
                                     <p>${products.category}</p>
-                                    <img src="${products.image}" alt="product image">
+                                    <img src="${products.image}" alt="image of ${products.title}">
                                     <h4>Precio: $ ${products.price.toFixed(2)}</h4> 
                                 </div>`        // Agregue toFixed(2) para que los precios queden con decimales
     })
